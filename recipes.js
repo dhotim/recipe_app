4985e3f68deeead1393d0f7af0d61d959219a36a
christerwpersson <christer.persson@gmail.com>
2021-07-06 08:21:52 +0200

    Require minimum length for recipe properties

--- recipes.js
+++ recipes.js
@@ -83,12 +83,15 @@ const recipeSchema = {
   properties: {
     title: {
       type: "string",
+      minLength: 1,
     },
     description: {
       type: "string",
+      minLength: 1,
     },
     instructions: {
       type: "string",
+      minLength: 1,
     },
     ingredients: {
       type: "array",
